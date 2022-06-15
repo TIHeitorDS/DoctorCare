889350277d51ea5f20465a2f2e8ac04dea02b5cc
TIHeitorDS <105075732+TIHeitorDS@users.noreply.github.com>
2022-06-15 13:18:45 -0300

    repairing showNavOnScroll()

--- main.js
+++ main.js
@@ -33,10 +33,11 @@ function activateMenuAtCurrentSection(section) {
 }
 
 function showNavOnScroll() {
+  const $navigation = document.getElementById('navigation')
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    $navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll')
+    $navigation.classList.remove('scroll')
   }
 }
 
